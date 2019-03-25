185d2e1ba2506cdc1f94b5da78549436fc9751f2
gopuvenkat <Gopalakrishnan.V@iiitb.org>
2019-03-26 02:07:29 +0530

    initial commit - lda

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ from matplotlib import pyplot as plt
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.decomposition import PCA
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 from sklearn.model_selection import cross_val_score
 
 FILEPATH = './NewData/'
@@ -14,11 +15,11 @@ FILEPATH = './NewData/'
 class Model:
     def __init__(self, file_path, n_components):
         self.pca = PCA(n_components=n_components, whiten=True)
-        self.logistic_regression = LogisticRegression()
         self.file_path = file_path
         self.category_list = self.getCategories()
         self.data = self.getData()
         self.data_ravell = self.generate_ravell()
+        self.data_list = self.generate_data_list()
         self.labels = self.generate_labels()
 
     def getCategories(self):
@@ -31,11 +32,16 @@ class Model:
             images = []
             for file in os.listdir(folder_path):
                 file_path = os.path.join(folder_path, file)
-                # read as grayscale image
                 img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
                 images.append(img)
             data[folder] = images
         return data
+
+    def generate_data_list(self):
+        data_list = list()
+        for cat in self.category_list:
+            data_list.extend(self.data[cat])
+        return np.array(data_list)
 
     def generate_ravell(self):
         def ravell(img):
@@ -54,7 +60,7 @@ class Model:
             plt.imshow(mean_eigen_face, 'gray')
             plt.show()
             for i, face in enumerate(eigen_faces):
-                plt.subplot(5, 5, 1+i)
+                plt.subplot(10, 20, 1+i)
                 plt.imshow(face, 'gray')
                 plt.xticks(list())
                 plt.yticks(list())
@@ -63,13 +69,32 @@ class Model:
     def generate_labels(self):
         labels = list()
         for cat in self.category_list:
-            labels += [cat]*len(self.data[cat])
-        return labels
+            labels += [0]*len(self.data[cat])
+        return np.array(labels).reshape(-1, 1)
 
-    def predict_logistic(self):
-        scores = cross_val_score(self.logistic_regression, self.pca_X, self.labels, cv=5, scoring='accuracy')
-        print(scores)
+    def model_predict(self, models=list()):
+        for model in models:
+            scores = cross_val_score(model, self.pca_X, self.labels, cv=5, scoring='accuracy')
+            print(model.__class__.__name__, np.mean(scores))
 
-model = Model(file_path=FILEPATH, n_components=25)
-model.perform_pca(show=False)
-model.predict_logistic()
+    def perform_lda(self):
+        clf = LinearDiscriminantAnalysis()
+        clf.fit(self.data_ravell, self.labels)
+        scores = cross_val_score(clf, self.data_ravell, self.labels, cv=5, scoring='accuracy')
+        print(clf.__class__.__name__, np.mean(scores))
+
+    def perform_lbp(self):
+        lbp = cv2.face.LBPHFaceRecognizer_create()
+        print(len(self.data_list))
+        print(len(self.labels))
+        lbp.train(self.data_list, self.labels)
+        lbp.predict(self.data_list)
+
+model = Model(file_path=FILEPATH, n_components=200)
+# model.perform_pca(show=True)
+# models = list()
+# models.append(LogisticRegression())
+# models.append(SVC(kernel='linear'))
+# model.model_predict(models=models)
+# model.perform_lda()
+model.perform_lbp()
